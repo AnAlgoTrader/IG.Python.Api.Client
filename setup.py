0c440edd8a4d21b992a4a4069ba6743946e4befb
oneangrytrader <one.angry.trader@gmail.com>
2020-10-13 15:22:55 +0100

    no message

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='IG.Python.Api.Client',
+      version='1.0',
+      description='IG Markets Python client',
+      author='One Angry Trader',
+      author_email='one.angry.trader@gmail.com',
+      url='https://github.com/oneangrytrader/IG.Python.Api.Client',
+      packages=['distutils', 'distutils.command'],
+     )

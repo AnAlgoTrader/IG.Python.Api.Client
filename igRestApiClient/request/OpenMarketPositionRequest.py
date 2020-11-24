4b22e0c5b262288b284bc49b2036cdc5381be576
oneangrytrader <one.angry.trader@gmail.com>
2020-11-24 04:19:50 +0000

    .

--- igRestApiClient/request/OpenMarketPositionRequest.py
+++ igRestApiClient/request/OpenMarketPositionRequest.py
@@ -1,15 +1,10 @@
-from igRestApiClient.model.enum.Currency import Currency
-from igRestApiClient.model.enum.Expiry import Expiry
-from igRestApiClient.model.enum.OrderType import OrderType
-
-
 class OpenMarketPositionRequest:
     epic = None
     direction = None
     size = None
-    expiry = Expiry.DFB.value
-    orderType = OrderType.Market.value
+    expiry = 'DFB'
+    orderType = 'MARKET'
     guaranteedStop = False,
     trailingStop = False,
     forceOpen = True,
-    currencyCode = Currency.GBP.value
+    currencyCode = 'GBP'

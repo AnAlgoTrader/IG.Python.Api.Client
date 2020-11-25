14f36b18d3ed40e9cac7df1ae1f5af7e3c476dd1
oneangrytrader <one.angry.trader@gmail.com>
2020-11-25 05:42:40 +0000

    .

--- igRestApiClient/response/OpenPositionResponse.py
+++ igRestApiClient/response/OpenPositionResponse.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, TypeVar, Type, cast
-
+from typing import Optional, Any, TypeVar, Type, cast
 
 T = TypeVar("T")
 
@@ -10,6 +9,20 @@ def from_str(x: Any) -> str:
     return x
 
 
+def from_none(x: Any) -> Any:
+    assert x is None
+    return x
+
+
+def from_union(fs, x):
+    for f in fs:
+        try:
+            return f(x)
+        except:
+            pass
+    assert False
+
+
 def to_class(c: Type[T], x: Any) -> dict:
     assert isinstance(x, c)
     return cast(Any, x).to_dict()
@@ -17,18 +30,19 @@ def to_class(c: Type[T], x: Any) -> dict:
 
 @dataclass
 class OpenPositionResponse:
-    deal_reference: str
-    error_code: str
+    deal_reference: Optional[str] = None
+    error_code: Optional[str] = None
 
     @staticmethod
     def from_dict(obj: Any) -> 'OpenPositionResponse':
         assert isinstance(obj, dict)
-        deal_reference = from_str(obj.get("dealReference"))
-        error_code = from_str(obj.get("errorCode"))
+        deal_reference = from_union([from_str, from_none], obj.get("dealReference"))
+        error_code = from_union([from_str, from_none], obj.get("errorCode"))
         return OpenPositionResponse(deal_reference, error_code)
 
     def to_dict(self) -> dict:
-        result: dict = {"dealReference": from_str(self.deal_reference), "errorCode": from_str(self.error_code)}
+        result: dict = {"dealReference": from_union([from_str, from_none], self.deal_reference),
+                        "errorCode": from_union([from_str, from_none], self.error_code)}
         return result
 
 
